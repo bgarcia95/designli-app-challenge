3faea8e43cc710fc57bc92def9052aa5fe7314c1
Brayan Garcia <brayan.garcia@wotdev.com>
2025-01-21 06:38:00 -0600

    feat: add type declarations for dotenv

---
+++ types/env.d.ts
@@ -0,0 +1,6 @@
+declare module '@env' {
+    export const API_URL: string;
+    export const FINNHUB_API_KEY: string;
+    export const AUTH0_DOMAIN: string;
+    export const AUTH0_CLIENT_ID: string;
+  }
